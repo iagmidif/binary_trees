2be5a178b22846ca2899e18b660d715677653ebf
Ngoamie <ngomeme28@gmail.com>
2022-08-24 13:10:57 -0700

    Is BST

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 
 #endif
